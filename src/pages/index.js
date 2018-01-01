f4674ab1d42e67a60e0980f0a1e7dc328eea5e8c
Ariel Gerstein <ariel.ger@hotmail.com>
2018-01-01 19:53:46 -0300

    Initial commit, add example to show styled-jsx bug in prod

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+
+const IndexPage = () => (
+  <div>
+    <a href="#">Example link</a>
+
+    <style jsx global>{`
+      a {
+        color: red;
+      }
+    `}</style>
+  </div>
+);
+
+export default IndexPage;
